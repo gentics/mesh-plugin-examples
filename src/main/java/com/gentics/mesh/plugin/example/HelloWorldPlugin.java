2d51fbbb7dabb28d09950618af8ec49b78e91ae4
Johannes Schüth <j.schueth@gentics.com>
2018-03-06 20:22:50 +0100

    Add osgi changes

--- src/main/java/com/gentics/mesh/plugin/example/HelloWorldPlugin.java
+++ src/main/java/com/gentics/mesh/plugin/example/HelloWorldPlugin.java
@@ -1,18 +1,20 @@
 package com.gentics.mesh.plugin.example;
 
+import org.osgi.framework.BundleContext;
+
 import com.gentics.mesh.plugin.rest.AbstractPlugin;
 
 public class HelloWorldPlugin extends AbstractPlugin {
 
 	@Override
-	public void start() {
-		// Note that an extension will be deployed multiple times 
+	public void start(BundleContext context) {
+		// Note that an extension will be deployed multiple times
 		// and thus constructed multiple times.
 		addExtension(() -> new HelloWorldRestExtension());
 	}
 
 	@Override
-	public void stop() {
+	public void stop(BundleContext context) {
 
 	}
 
