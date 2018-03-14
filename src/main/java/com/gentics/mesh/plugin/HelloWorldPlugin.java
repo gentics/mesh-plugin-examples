acd76abdb73dc617ee9938321371ba50bf6b427d
Johannes Schüth <git@jotschi.de>
2018-03-14 21:39:54 +0100

    wip

--- src/main/java/com/gentics/mesh/plugin/HelloWorldPlugin.java
+++ src/main/java/com/gentics/mesh/plugin/HelloWorldPlugin.java
@@ -7,16 +7,25 @@ import com.gentics.mesh.plugin.rest.AbstractPlugin;
 public class HelloWorldPlugin extends AbstractPlugin {
 
 	@Override
+	public String getName() {
+		return "hello world plugin";
+	}
+
+	@Override
 	public void start(BundleContext context) {
-		System.out.println("Start");
+		System.out.println("Starting " + getName());
 		// Note that an extension will be deployed multiple times
 		// and thus constructed multiple times.
 		addExtension(() -> new HelloWorldRestExtension());
+
+		System.out.println("Add service on {" + getName() + "}");
+		context.registerService(Plugin.class, this, null);
+
 	}
 
 	@Override
 	public void stop(BundleContext context) {
-		System.out.println("Stop");
+		System.out.println("Stoping " + getName());
 	}
 
 }
