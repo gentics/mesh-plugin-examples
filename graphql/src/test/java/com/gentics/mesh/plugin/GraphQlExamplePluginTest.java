86c5d850abbb68efef7865da791e0343ddd0a873
Johannes Schüth <j.schueth@gentics.com>
2019-08-01 14:24:16 +0200

    Update example tests

--- graphql/src/test/java/com/gentics/mesh/plugin/GraphQlExamplePluginTest.java
+++ graphql/src/test/java/com/gentics/mesh/plugin/GraphQlExamplePluginTest.java
@@ -1,14 +1,18 @@
 package com.gentics.mesh.plugin;
 
+import java.io.IOException;
+
 import org.junit.ClassRule;
 import org.junit.Test;
 
 import com.gentics.mesh.Mesh;
+import com.gentics.mesh.core.rest.graphql.GraphQLResponse;
 import com.gentics.mesh.core.rest.project.ProjectCreateRequest;
 import com.gentics.mesh.rest.client.MeshRestClient;
 import com.gentics.mesh.test.local.MeshLocalServer;
 
 import io.reactivex.Completable;
+import io.reactivex.Single;
 
 public class GraphQlExamplePluginTest {
 
@@ -20,20 +24,26 @@ public class GraphQlExamplePluginTest {
 		.waitForStartup();
 
 	@Test
-	public void testPlugin() {
+	public void testPlugin() throws IOException {
 		Mesh mesh = server.getMesh();
 		MeshRestClient client = server.client();
 
 		// Create the project
-		Completable createProject = client.createProject(new ProjectCreateRequest().setName(PROJECT_NAME)).toCompletable();
-		// Deploy the plugin
-		Completable deployPlugin = mesh.deployPlugin(GraphQLExamplePlugin.class, "basic");
+		Completable createProject = client.createProject(new ProjectCreateRequest().setName(PROJECT_NAME).setSchemaRef("folder")).toCompletable();
 
-		Completable.mergeArray(createProject, deployPlugin)
-			.andThen(client.graphqlQuery(PROJECT_NAME, "{ pluginApi { graphql { text } } }").toSingle()).subscribe(result -> {
-				System.out.println(result.getData().encodePrettily());
-			}, err -> {
-				err.printStackTrace();
-			});
+		// Deploy the plugin
+		Completable deployPlugin = mesh.deployPlugin(GraphQLExamplePlugin.class, "myPlugin");
+
+		// Setup the instance
+		Completable setup = Completable.mergeArray(createProject, deployPlugin);
+
+		// Run query on the plugin
+		Single<GraphQLResponse> queryOnPlugin = client.graphqlQuery(PROJECT_NAME, "{ pluginApi { myPlugin { text } } }").toSingle();
+
+		// Run the operations
+		GraphQLResponse result = setup.andThen(queryOnPlugin).blockingGet();
+
+		// Print the GraphQL API result
+		System.out.println(result.getData().encodePrettily());
 	}
 }
---
+++ graphql/src/test/java/com/gentics/mesh/plugin/PluginRunnerExample.java
@@ -0,0 +1,48 @@
+package com.gentics.mesh.plugin;
+
+import com.gentics.mesh.Mesh;
+import com.gentics.mesh.OptionsLoader;
+import com.gentics.mesh.core.rest.project.ProjectCreateRequest;
+import com.gentics.mesh.etc.config.MeshOptions;
+import com.gentics.mesh.rest.client.MeshRestClient;
+
+import io.reactivex.Completable;
+
+public class PluginRunnerExample {
+
+	private static String PROJECT_NAME = "test";
+
+	public static void main(String[] args) {
+		MeshOptions options = OptionsLoader.generateDefaultConfig();
+		// Enable in-memory mode
+		options.getStorageOptions().setDirectory(null);
+
+		// Disable embedded ES
+		options.getSearchOptions().setUrl(null).setStartEmbedded(false);
+		options.getAuthenticationOptions().setKeystorePath("target/keystore_" + System.currentTimeMillis() + ".jceks");
+
+		MeshRestClient client = MeshRestClient.create("localhost", 8080, false);
+		Mesh mesh = Mesh.mesh(options);
+
+		// Deploy the plugin
+		Completable deployPlugin = mesh.deployPlugin(GraphQLExamplePlugin.class, "myPlugin");
+
+		// Login and create the needed project
+		client.setLogin("admin", "admin");
+		Completable clientLogin = client.login().toCompletable();
+		Completable createProject = Completable
+			.defer(() -> client.createProject(new ProjectCreateRequest().setName(PROJECT_NAME).setSchemaRef("folder")).toCompletable());
+
+		mesh.rxRun()
+			.andThen(clientLogin)
+			.andThen(Completable.mergeArray(createProject, deployPlugin)
+				.andThen(client.graphqlQuery(PROJECT_NAME, "{ pluginApi { myPlugin { text } } }").toSingle()))
+			.subscribe(result -> {
+				System.out.println(result.getData().encodePrettily());
+				mesh.shutdown();
+			}, err -> {
+				err.printStackTrace();
+			});
+	}
+
+}
